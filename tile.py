314018c872d70ee74b9095b90c9848bf7e8757c4
lazysnail0589 <33462355+lazysnail0589@users.noreply.github.com>
2022-05-04 21:01:36 -0500

    Update tile.py

--- tile.py
+++ tile.py
@@ -1,10 +1,28 @@
 import pygame
 from settings import *
+from entity import Entity
+from support import *
+#from Character import character
+#from main import Game
 
 class Tile(pygame.sprite.Sprite):
-     def __init__(self,pos,groups):
+     def __init__(self,pos,groups,sprite_type, surface = pygame.Surface((TILESIZE,TILESIZE))):
          super().__init__(groups)
-         self.image = pygame.image.load(r'C:\Users\Javen\PycharmProjects\pythonProject\dirt.png').convert_alpha()
+         self.sprite_type = sprite_type
+         self.image = pygame.image.load(r'C:\Users\Javen\PycharmProjects\pythonProject\dirt2.png').convert_alpha()
+
          self.rect = self.image.get_rect(topleft = pos)
+         self.hitbox = self.rect.inflate(0,20)
 
+class Coins(pygame.sprite.Sprite):
+    def __init__(self,pos,groups):
+         super().__init__(groups)
+         self.image = pygame.image.load(r'C:\Users\Javen\PycharmProjects\pythonProject\venv\coinandpeach\coin1.png').convert_alpha()
 
+         self.rect = self.image.get_rect(topleft = pos)
+         self.hitbox = self.rect.inflate(0,20)
+
+    #def coin_collision(self):
+        #if character.player.hitbox.colliderect(self.hitbox):
+            #del self.image
+            #Game.score += 1
